4a802081b57dcbe91af9690b9993d363fd3d6d30
Braulio <braulio@focusgte.com>
2022-09-08 20:06:34 -0600

    Rutas públicas y privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "../auth";
 import HeroesRoutes from "../heroes/routes/HeroesRoutes";
 import { Navbar } from "../ui";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="login" element={<LoginPage />} />
-                <Route path="/*" element={<HeroesRoutes />} />
+                {/* <Route path="login" element={<LoginPage />} /> */}
+                <Route path="/login" element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>} />
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <HeroesRoutes />
+                    </PrivateRoute>} />
             </Routes>
         </>
     )
